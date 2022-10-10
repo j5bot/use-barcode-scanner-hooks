662ae0300a4022dd70f5c59413e45492af445c37
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2022-10-10 00:23:01 -0400

    Messing around with creating components sub-package

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,48 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
+const makeDefaultConfig = (hooksOrComponents) => {
+    return [
+        {
+            input: `src/${hooksOrComponents}/index.ts`,
+            output: [
+                {
+                    file: packageJson[hooksOrComponents].main,
+                    format: "cjs",
+                    sourcemap: true,
+                },
+                {
+                    file: packageJson[hooksOrComponents].module,
+                    format: "esm",
+                    sourcemap: true,
+                },
+            ],
+            external: [ 'react', 'react-dom' ],
+            plugins: [
+                resolve(),
+                commonjs(),
+                typescript({ tsconfig: "./tsconfig.json" }),
+                excludeDependenciesFromBundle({ peerDependencies: true }),
+            ],
+        },
+        {
+            input: `dist/${hooksOrComponents}/esm/types/index.d.ts`,
+            output: [{ file: `dist/${hooksOrComponents}/index.d.ts`, format: "esm" }],
+            external: [ 'react', 'react-dom' ],
+            plugins: [
+                dts(),
+                excludeDependenciesFromBundle({ peerDependencies: true }),
+            ],
+        }
+    ];
+};
+
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        external: [ 'react', 'react-dom' ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-    },
+    ...makeDefaultConfig('hooks'),
+    ...makeDefaultConfig('components'),
 ];
