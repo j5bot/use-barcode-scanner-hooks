72043389e1aa0b923c90e7beaf319e619a8d389f
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-03-06 19:00:39 -0500

    upgrade rollup / dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,7 @@ import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-fr
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 
-const packageJson = require("./package.json");
+import packageJson from "./package.json" assert { type: "json" };
 
 const makeDefaultConfig = (hooksOrComponents) => {
     return [
